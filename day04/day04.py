fb2f19669ebcc0935b0e1b0cf524335f346b030d
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2021-12-07 22:22:49 +1100

    Complete Day 4 part 2

--- day04/day04.py
+++ day04/day04.py
@@ -1,4 +1,4 @@
-import math, os, re
+import re
 
 print("\n========== INPUTING FILE ==========")
 
@@ -25,8 +25,7 @@ for line in inFile:
 # print("List: " + str(myList) + "\n")
 
 print("\n========== Part 1 ==========")
-winningCard = 0
-winningNumber = 0
+winningCard, winningNumber = 0, 0
 bingoNumbers = [[0] * 5 for i in range(len(myList))]
 bingo = False
 for number in numbers:
@@ -34,15 +33,8 @@ for number in numbers:
         if number in myList[row]:
             index = myList[row].index(number)
             bingoNumbers[row][index] = 1 # Mark off each time number is 'numbers' appears in lists
-            # Check if there is 5 in a row
-            if 0 not in bingoNumbers[row]:
-                print("row success")
-                bingo = True
-                winningCard = row - row%5
-                break
-            # Check if there is 5 in a column
-            if bingoNumbers[row/5][index] ==1 and bingoNumbers[row/5+1][index]==1  and bingoNumbers[row/5+2][index]==1  and bingoNumbers[row/5+3][index]==1  and bingoNumbers[row/5+4][index]==1:
-                print("column success")
+            # Check if there is 5 in a row or column
+            if 0 not in bingoNumbers[row] or bingoNumbers[row - row%5][index] ==1 and bingoNumbers[row - row%5+1][index]==1  and bingoNumbers[row - row%5 + 2][index]==1  and bingoNumbers[row - row%5+3][index]==1  and bingoNumbers[row - row%5+4][index]==1:
                 bingo = True
                 winningCard = row - row%5
                 break
@@ -54,6 +46,49 @@ for number in numbers:
 totalWinningBoard = 0
 for row in range(winningCard,winningCard+5):
     for column in range(5):
+        if bingoNumbers[row][column] == 0:
+            totalWinningBoard += myList[row][column]
+
+# Calculation
+print(winningNumber * totalWinningBoard)
+
+print("\n========== Part 2 ==========")
+
+def removeCard(row):
+    for i in reversed(range(row, row+5)):
+        myList.pop(i)
+        bingoNumbers.pop(i)
+
+winningCard, winningNumber = 0, 0
+boardsRemaining = len(myList)/5
+bingoNumbers = [[0] * 5 for i in range(len(myList))]
+bingo = False
+
+for number in numbers:
+    cardsToDelete = []
+    for row in range(len(myList)):
+        if number in myList[row]:
+            index = myList[row].index(number)
+            bingoNumbers[row][index] = 1 # Mark off each time number is 'numbers' appears in lists
+            # Check if there is 5 in a row or column
+            if 0 not in bingoNumbers[row] or bingoNumbers[row - row%5][index] ==1 and bingoNumbers[row - row%5+1][index]==1  and bingoNumbers[row - row%5+2][index]==1  and bingoNumbers[row - row%5+3][index]==1  and bingoNumbers[row - row%5+4][index]==1:
+                winningCard = row - row%5
+                if(boardsRemaining == 1):
+                    bingo = True
+                    break
+                boardsRemaining -=1
+                cardsToDelete.append(winningCard)
+                row += 5
+    for item in reversed(cardsToDelete): # Remove list of cards that have bingo
+        removeCard(item)
+    if bingo == True:
+        winningNumber = number
+        break
+
+# Find sum of unmarked numbers on winning Card
+totalWinningBoard = 0
+for row in range(0,5):
+    for column in range(0, 5):
         if bingoNumbers[row][column] == 0:
             totalWinningBoard += myList[row][column]
 
