435aa1fad2c1d35aaa593ab4a15a6d8204a8e349
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2021-12-08 21:26:08 +1100

    Complete Day 5 part 1

--- day05/day05.py
+++ day05/day05.py
@@ -1,4 +1,4 @@
-import math, re, os
+import re, math
 
 print("\n========== INPUTING FILE ==========")
 
@@ -22,16 +22,70 @@ for line in inFile:
 # print("Number of Items: " + str(output))
 # print("List: " + str(myList) + "\n")
 
+# def getPointsOnLine(points, _list):
+#     if points[0] == points[2]: # Vertical Line
+#         _min = min(points[1], points[3])
+#         _max = max(points[1], points[3])
+#         for y in range(min, max+1):
+#             if (points[0], y) in coordinates:
+#                 coordinates[(points[0], y)] += 1
+#             else:
+#                 coordinates[(points[0], y)] = 1
+#     elif points[1] == points[3]: # Horizontal Line
+#         _min = min(points[0], points[2])
+#         _max = max(points[0], points[2])
+#         for x in range(min, max+1):
+#             if (x, points[1]) in coordinates:
+#                 coordinates[(x, points[1])] += 1
+#             else:
+#                 coordinates[(x, points[1])] = 1
+#     else:
+#         gradient = (points[1] - points[3]) / (points[0]-points[2])
+#         if gradient > 0:
+#             _min = min(points[1], points[3])
+#             _max = max(points[1], points[3])
+#             for y in range(min, max+1):
+#                 if (points[0], y) in coordinates:
+#                     coordinates[(points[0], y)] += 1
+#                 else:
+#                     coordinates[(points[0], y)] = 1
+#             print("pos")
+#         elif gradient < 0:
+#             print("neg")       
+#         else:
+#             print("straight")
+#         return [] # Return a list of tuples
+
 print("\n========== Part 1 ==========")
 coordinates = {}
 
-# TODO: Need a more efficient way of doing it!!!!
 for coords in myList:
-    for x in range(coords[0], coords[2]+1):
-        for y in range(coords[1], coords[3]+1):
-            if (x, y) in coordinates:
-                coordinates[(x, y)] += 1
+    if coords[0] == coords[2]: # Vertical Line
+        _min = min(coords[1], coords[3])
+        _max = max(coords[1], coords[3])
+        for y in range(_min, _max+1):
+            if (coords[0], y) in coordinates:
+                coordinates[(coords[0], y)] += 1
             else:
-                coordinates[(x, y)] = 1
+                coordinates[(coords[0], y)] = 1
+        print("VERTICAL")
+    elif coords[1] == coords[3]: # Horizontal Line
+        _min = min(coords[0], coords[2])
+        _max = max(coords[0], coords[2])
+        for x in range(_min, _max+1):
+            if (x, coords[1]) in coordinates:
+                coordinates[(x, coords[1])] += 1
+            else:
+                coordinates[(x, coords[1])] = 1
+        print("HORIZONTAL")
 
-print(coordinates)
+# print(coordinates)
+counter = 0
+for item in coordinates:
+    # print(coordinates[item])
+    if coordinates[item] >= 2:
+        counter +=1
+
+print(counter)
+
+print("\n========== Part 2 ==========")
