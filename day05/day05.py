b2df29a238a3548877a6e5788387a4b0a15239dd
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2021-12-08 21:48:43 +1100

    Complete Day 5 part 2

--- day05/day05.py
+++ day05/day05.py
@@ -22,40 +22,6 @@ for line in inFile:
 # print("Number of Items: " + str(output))
 # print("List: " + str(myList) + "\n")
 
-# def getPointsOnLine(points, _list):
-#     if points[0] == points[2]: # Vertical Line
-#         _min = min(points[1], points[3])
-#         _max = max(points[1], points[3])
-#         for y in range(min, max+1):
-#             if (points[0], y) in coordinates:
-#                 coordinates[(points[0], y)] += 1
-#             else:
-#                 coordinates[(points[0], y)] = 1
-#     elif points[1] == points[3]: # Horizontal Line
-#         _min = min(points[0], points[2])
-#         _max = max(points[0], points[2])
-#         for x in range(min, max+1):
-#             if (x, points[1]) in coordinates:
-#                 coordinates[(x, points[1])] += 1
-#             else:
-#                 coordinates[(x, points[1])] = 1
-#     else:
-#         gradient = (points[1] - points[3]) / (points[0]-points[2])
-#         if gradient > 0:
-#             _min = min(points[1], points[3])
-#             _max = max(points[1], points[3])
-#             for y in range(min, max+1):
-#                 if (points[0], y) in coordinates:
-#                     coordinates[(points[0], y)] += 1
-#                 else:
-#                     coordinates[(points[0], y)] = 1
-#             print("pos")
-#         elif gradient < 0:
-#             print("neg")       
-#         else:
-#             print("straight")
-#         return [] # Return a list of tuples
-
 print("\n========== Part 1 ==========")
 coordinates = {}
 
@@ -68,7 +34,6 @@ for coords in myList:
                 coordinates[(coords[0], y)] += 1
             else:
                 coordinates[(coords[0], y)] = 1
-        print("VERTICAL")
     elif coords[1] == coords[3]: # Horizontal Line
         _min = min(coords[0], coords[2])
         _max = max(coords[0], coords[2])
@@ -77,15 +42,72 @@ for coords in myList:
                 coordinates[(x, coords[1])] += 1
             else:
                 coordinates[(x, coords[1])] = 1
-        print("HORIZONTAL")
 
-# print(coordinates)
 counter = 0
 for item in coordinates:
-    # print(coordinates[item])
     if coordinates[item] >= 2:
         counter +=1
 
 print(counter)
 
 print("\n========== Part 2 ==========")
+
+def addStraightLine(coordinates, v1, v2, v3, mode):
+    _min = min(v1, v2)
+    _max = max(v1, v2)
+    for xy in range(_min, _max+1):
+        if mode == 'v':
+            if (v3, xy) in coordinates:
+                coordinates[(v3, xy)] += 1
+            else:
+                coordinates[(v3, xy)] = 1
+        else:
+            if (xy, v3) in coordinates:
+                coordinates[(xy, v3)] += 1
+            else:
+                coordinates[(xy, v3)] = 1
+
+coordinates = {}
+for points in myList:
+    if points[0] == points[2]: # Vertical Line
+        addStraightLine(coordinates, points[1], points[3], points[0], 'v')
+    elif points[1] == points[3]: # Horizontal Line
+        addStraightLine(coordinates, points[0], points[2], points[1], 'h')
+    else: # Diagonal Line
+        gradient = (points[1] - points[3]) / (points[0]-points[2])
+        if gradient > 0: # Positive Gradient start on lowest Y
+            if points[1] < points[3]:
+                _min = points[1]
+                x = points[0]
+                _max = points[3]
+            else:
+                _min = points[3]
+                x = points[2]
+                _max = points[1]
+            for y in range(_min, _max+1):
+                if (x, y) in coordinates:
+                    coordinates[(x, y)] += 1
+                else:
+                    coordinates[(x, y)] = 1
+                x +=1
+        else : # Negative Gradient, Start on lowest X, decrease the Y value.
+            if points[0] < points[2]:
+                _min = points[0]
+                y = points[1]
+                _max = points[2]
+            else:
+                _min = points[2]
+                y = points[3]
+                _max = points[0]
+            for x in range(_min, _max+1):
+                if (x, y) in coordinates:
+                    coordinates[(x, y)] += 1
+                else:
+                    coordinates[(x, y)] = 1
+                y -=1
+counter = 0
+for item in coordinates:
+    if coordinates[item] >= 2:
+        counter +=1
+
+print(counter == 19851)
