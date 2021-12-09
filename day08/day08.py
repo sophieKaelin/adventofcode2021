435522843442ee98481057509044a76058631f6f
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2021-12-09 23:26:33 +1100

    Optimise code (and make less readable in process)

--- day08/day08.py
+++ day08/day08.py
@@ -1,51 +1,49 @@
+import collections
+
 print("\n========== INPUTING FILE ==========")
 
 inFile = open("input.txt")
 myList = []
 
+
 for line in inFile:
+    # Input line
     temp = line.replace("\n","")
     temp = temp.split("|")
+    # Signal Pattern sorting
     temp[0] = temp[0].split(" ")
     temp[0].pop(10)
     temp[0] = sorted(temp[0], key=len)
+
     for i in range(10): # Sort values so they are alphabetical 
         temp[0][i] = "".join(sorted(temp[0][i]))
+    
+    # Output value sorting
     temp[1] = temp[1].split(" ")
     temp[1].pop(0)
     for i in range(4):
         temp[1][i] = "".join(sorted(temp[1][i]))
-    myList.append(temp)
+    myList.append(temp) # Add items to list
 
 # print("List: " + str(myList) + "\n")
 
-print("\n========== SOLUTION ==========")
+# ========== SOLUTION ==========
 
 counter1 = 0
 counter2 = 0
+
 for line in myList:
     numbers = {1:line[0][0], 7:line[0][1], 4:line[0][2], 8:line[0][9]} # Default, must be these values
     characters = {}
-    # Find item is "2" -> 1 character from "1" is common in all but 1. This character is "c"
-    first, second = [], []
-    char1, char2 = line[0][0][0], line[0][0][1]
 
-    for index in range(len(line[0])):
-        if char1 not in line[0][index]:
-            first.append(index)
-        if char2 not in line[0][index]:
-            second.append(index)
-    if len(first) == 1:
-        numbers[2] = line[0][first[0]]
-        characters['c'] = char1
-    else:
-        numbers[2] = line[0][second[0]]
-        characters['c'] = char2
+    # Find 'a' = chars in '7' minus chars in '1'
+    characters['a'] = numbers[7].translate(None, ''.join(list(numbers[1])))
 
-    # Find F -> whatever is missing from "2" characters + 'c'
-    characters['f'] = "abcdefg".translate(None, ''.join(list(numbers[2]))).replace(characters['c'],"")
+    # Find 'c' = 1 character from "1" is common in all but 1 combo ('c' not in "2")
+    combined = collections.Counter("".join(line[0]))
+    characters['c'] = combined.keys()[combined.values().index(9)]
 
-    # "1" characters - "c" character = "b"
+    # Find 'b' = characters in "1" - 'c'
     characters['b'] = numbers[1].replace(characters['c'], "")
 
     # length = 5, which one doesn't have b value is 5. Other letter missing is 'e'
@@ -53,10 +51,13 @@ for line in myList:
         if characters['b'] not in line[0][index]:
             numbers[5] = line[0][index]
             characters['e'] = "abcdefg".translate(None, ''.join(list(numbers[5]))).replace(characters['b'],"")
-        elif numbers[2] == line[0][index]:
-            continue
+        elif characters['c'] not in line[0][index]:
+            numbers[2] = line[0][index]
         else:
             numbers[3] = line[0][index]
+    
+    # Find F -> whatever is missing from "2" characters + 'c'
+    characters['f'] = "abcdefg".translate(None, ''.join(list(numbers[2]))).replace(characters['c'],"")
 
     # length = 6, which one doesn't have b value is '6'. which one doesn't have e value is '9'. Which ever is left is '0' and the one missing is "g"
     for index in range(6, 9):
@@ -67,10 +68,7 @@ for line in myList:
         else:
             numbers[0] = line[0][index]
             characters['g'] = "abcdefg".translate(None, ''.join(list(numbers[0])))
-
-    # Find 'a' = chars in '7' minus chars in '1'
-    characters['a'] = numbers[7].translate(None, ''.join(list(numbers[1])))
-
+    
     # Find D. Remove 'a' from '9'. compare '4' and '9' which ever one is different is D
     characters['d'] = numbers[9].translate(None, ''.join(list(numbers[4]))).replace(characters['a'],'')
 
