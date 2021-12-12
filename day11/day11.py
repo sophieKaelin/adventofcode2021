00f7b38112a1bf4620b52b94d85df96598a946cd
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2021-12-12 22:44:56 +1100

    Fix up day11

--- day11/day11.py
+++ day11/day11.py
@@ -10,65 +10,7 @@ for line in inFile:
 
 print("List: " + str(myList) + "\n")
 
-print("\n========== Part 1 ==========")
-
-# def getAdjacent(i, myList):
-#     adj = []
-#     r = i/10
-#     c = i%10
-#     if r == 0 and c == 0: # Top Left
-#         adj.extend([i+1,i+11,i+10])
-#     elif r == 0 and c == 9: # Top Right
-#         adj.extend([i-1,i+9,i+10])
-#     elif r == 9 and c == 0: # Bottom Left
-#         adj.extend([i+1,i-9,i-10])
-#     elif r == 9 and c == 9: # Bottom Right
-#         adj.extend([i-1,i-11,i-10])
-#     elif c == 0:
-#         adj.extend([i+1, i-10, i+10, i-9, i + 11])
-#     elif c == 9:
-#         adj.extend([i-1, i-10, i+10, i+9, i -11])
-#     elif r == 0:
-#         adj.extend([i-1, i+1, i+10, i+9, i + 11])
-#     elif r == 9:
-#         adj.extend([i-1, i+1, i-10, i-9, i - 11])
-#     else:
-#         adj.extend([i-1, i+1, i-10, i+10, i-11, i+9, i + 11, i-9])
-#     return [i for i in adj if i >= 0 and i < 100]
-
-# def shineAdjacent(i, myList):
-#     adj = getAdjacent(i, myList)
-#     adj = [item for item in adj if item not in flashed]
-#     for index in adj:
-#         if myList[index] == 10:
-#             continue
-#         elif myList[index] == 9:
-#             myList[index] +=1
-#             flashed.append(index)
-#             shineAdjacent(index, myList)
-#         else:
-#             myList[index] += 1
-
-# flashes = 0
-# for loop in range(100):
-#     flashed = []
-#     for i in range(len(myList)):
-#         if i not in flashed: # Skip if already flashing
-#             myList[i] += 1
-#             if myList[i] == 10:
-#                 flashed.append(i)
-#                 shineAdjacent(i, myList)
-
-#     flashes += len(flashed)
-#     for i in flashed:
-#         myList[i] = 0   
-
-# print(sorted(flashed))
-# print(flashes)
-
-print("\n========== Part 2 ==========")
-
-def getAdjacent(i, myList):
+def getAdjacent(i):
     adj = []
     r = i/10
     c = i%10
@@ -93,7 +35,7 @@ def getAdjacent(i, myList):
     return [i for i in adj if i >= 0 and i < 100]
 
 def shineAdjacent(i, myList):
-    adj = getAdjacent(i, myList)
+    adj = getAdjacent(i)
     adj = [item for item in adj if item not in flashed]
     for index in adj:
         if myList[index] == 10:
@@ -104,6 +46,27 @@ def shineAdjacent(i, myList):
             shineAdjacent(index, myList)
         else:
             myList[index] += 1
+
+print("\n========== Part 1 ==========")
+
+flashes = 0
+for loop in range(100):
+    flashed = []
+    for i in range(len(myList)):
+        if i not in flashed: # Skip if already flashing
+            myList[i] += 1
+            if myList[i] == 10:
+                flashed.append(i)
+                shineAdjacent(i, myList)
+
+    flashes += len(flashed)
+    for i in flashed:
+        myList[i] = 0   
+
+print(sorted(flashed))
+print(flashes)
+
+print("\n========== Part 2 ==========")
 
 loop = 0
 while True:
