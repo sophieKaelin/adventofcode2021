82027ed7c8e523ae4a68e074deb79749acf584c5
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2021-12-14 10:14:28 +1100

    Complete day 9 part 1

--- day09/day09.py
+++ day09/day09.py
@@ -1,4 +1,4 @@
-import sys
+import sys, copy
 print("\n========== INPUTING FILE ==========")
 
 inFile = open("input.txt")
@@ -15,40 +15,50 @@ rows = len(myList)
 print("List: " + str(myList) + "\n")
 
 print("\n========== Part 1 ==========")
-smallest = []
-def inRange(r, c):
+smallest = [] # Indexes of smallest numbers
+
+def inRange(r, c): # Checks if the index is valid
     if (r >= 0 and r < rows) and (c >= 0 and c < cols):
         return myList[r][c]
     else:
         return sys.maxint
 
-riskLevelSum = 0
+def getSurrounding(r, c, _list): # Will get surrounding items and confirms they also exist in list
+    surrounding = []
+    if r-1 >= 0 and r-1 < rows and [r-1,c] in _list:
+        surrounding.append([r-1,c])
+    if r+1 >= 0 and r+1 < rows and [r+1,c] in _list:
+        surrounding.append([r+1,c])
+    if c-1 >= 0 and c-1 < cols and [r,c-1] in _list:
+        surrounding.append([r,c-1])
+    if c+1 >= 0 and c+1 < cols and [r,c+1] in _list:
+        surrounding.append([r,c+1])
+    return surrounding
+
 # Get all the smallest by comparing neighbors
 for r in range(rows):
     for c in range(cols):
-        surrounding = [inRange(r-1, c), inRange(r+1, c), inRange(r, c-1), inRange(r, c+1)]
-        if all(x > myList[r][c] for x in surrounding): # If [r][c] is smaller than each in the list
-            smallest.append(r*10 + c)
+        surrounding = [inRange(r-1, c),inRange(r+1, c),inRange(r, c-1),inRange(r, c+1)]
+        if len(surrounding) == 1 or all(x > myList[r][c] for x in surrounding): # If [r][c] is smaller than each in the list
+            smallest.append([r,c])
 
-print(smallest)
+# Check if there are any numbers in smallest which are adjacent, and only keep the smallest of those
+smallestOfSmallest = []
+print(str(smallest)+"\n")
+for coord in smallest:
+    surrounding = getSurrounding(coord[0], coord[1], smallest)
+    if len(surrounding) == 0 or all(x > myList[r][c] for x in surrounding): # If [r][c] is smaller than each in the list
+        smallestOfSmallest.append([coord[0],coord[1]])
 
-# Check if there are any numbers in smallest which are adjacent
-# for i in reversed(smallest):
-    # smallest = True
-    # if smallest[i] - 1 in smallest:
-        
-    # Check if there are any values smallest[i] - 1, smallest[i] + 1, smallest[i]-rows, smallest[i]+rows
-    # if not smallest:
-    #     smallest.remove(i)
-    # If item is not smallest, delete.
-    # print("doing something later")
+print(smallestOfSmallest)
 
 # Calculate Risk Level Sum
-for i in smallest:
-    riskLevelSum += myList[i/10][i%10]
-riskLevelSum += len(smallest)
+riskLevelSum = 0
+for i in smallestOfSmallest:
+    riskLevelSum += myList[i[0]][i[1]]
+riskLevelSum += len(smallestOfSmallest)
 
-print(riskLevelSum == 15)
+print(riskLevelSum)
 
 # Get list of smallest values
 # Then, loop through list of indexes, check if there is any adjacent, if there is, only keep the smallest
